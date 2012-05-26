dcf034143991ac64b7b2a932df1933963c0848ef
Kim DH <polae2@msn.com>
2012-05-26 17:48:56 +0900

    livelist add

--- TenVersion_0526/src/com/smartschool/tenversion/LiveListActivity.java
+++ TenVersion_0526/src/com/smartschool/tenversion/LiveListActivity.java
@@ -1,22 +1,58 @@
 package com.smartschool.tenversion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class LiveListActivity extends Activity  implements OnClickListener{
-    /** Called when the activity is first created. */
-    @Override
+	String[] lives = {"ª«ªﬂ π‰¡÷±‚","»≠∫– π∞¡÷±‚","ø≠¥ÎæÓ π‰¡÷±‚"};
+	
+	ArrayAdapter<String> adapter; 
+	ArrayList<String> liveList;
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.livelist);
-  
+          
+        liveList = new ArrayList<String>();
+        liveList.addAll(Arrays.asList(lives));
+        
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, liveList);
+        
+        ListView list = (ListView) findViewById(R.id.live_listview);
+        list.setAdapter(adapter);
+        
+        Button addButton = (Button) findViewById(R.id.add_btn);
+        addButton.setOnClickListener(this);
+        
+        Button delButton = (Button) findViewById(R.id.del_btn);
+        //delButton.setOnClickListener(this);
+        
     }
 
 	public void onClick(View v) {
+		Intent intent = null;
+		switch(v.getId()) {
+		case R.id.add_btn:
+			intent = new Intent(this, LiveListAddActivity.class);
+			startActivity(intent);
+			break;
 
-		
+		case R.id.del_btn:
+			intent = new Intent(this, LIveListDelActivity.class);
+			startActivity(intent);
+			break;
+			
+		}
 	}
 }
