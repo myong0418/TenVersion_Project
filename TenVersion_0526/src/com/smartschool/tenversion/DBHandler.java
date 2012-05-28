6b4882f0a594e994610dedf210c592c53a178973
myong <gyeong.mh@gmail.com>
2012-05-28 22:49:44 +0900

    add DBhandler,DBhelper

---
+++ TenVersion_0526/src/com/smartschool/tenversion/DBHandler.java
@@ -0,0 +1,85 @@
+package com.smartschool.tenversion;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+
+
+public class DBHandler {
+	public static final String TAG = "DBHandler";
+
+	private DBHelper helper;
+	private SQLiteDatabase db;
+
+	public static final String TABLE_NAME = "checklist_table";
+	public static final String KEY_ROWID = "_id";
+	public static final String KEY_MODE = "mode";			//1=safe, 2=live, 3=etc
+	public static final String KEY_LIST_DATA = "list_data";
+    
+    private DBHandler(Context ctx) {
+        this.helper = new DBHelper(ctx);
+        this.db = helper.getWritableDatabase();
+    }
+
+    public static DBHandler open(Context ctx) throws SQLException {
+        DBHandler handler = new DBHandler(ctx);        
+
+        return handler;    
+    }
+    
+    public void close() {
+        helper.close();
+    }
+
+    public long insert(String mode, String listData) {
+    	Log.v(TAG,"insert()  mode::"+mode+" , list_data ::"+listData);
+        ContentValues values = new ContentValues();
+        values.put(KEY_MODE,mode);       
+        values.put(KEY_LIST_DATA, listData);        
+
+        return db.insert(TABLE_NAME, null, values);
+    } 
+    
+    public boolean delete(long rowID){    
+    	Log.v(TAG,"delete()  rowID::"+rowID);
+        return db.delete(TABLE_NAME, KEY_ROWID + "=" + rowID, null) > 0;
+    } 
+
+    public boolean update(long rowID, String mode, String listData){
+    	Log.v(TAG,"update()  rowID::"+rowID+", mode::"+mode+" ,listData::"+listData);
+    	ContentValues values = new ContentValues();
+    	values.put(KEY_MODE, mode);
+    	values.put(KEY_LIST_DATA, listData);
+    	
+    	return db.update(TABLE_NAME, values, KEY_ROWID + "=" + rowID, null) > 0;
+    } 
+
+    public Cursor select(int id) throws SQLException { 
+    	Log.v(TAG,"select()  id::"+id);
+        Cursor cursor = db.query(true, TABLE_NAME, 
+                                new String[] {KEY_ROWID, KEY_MODE, KEY_LIST_DATA},
+                                KEY_ROWID + "=" + id, 
+                                null, null, null, null, null);        
+        if (cursor != null) { cursor.moveToFirst(); }        
+
+        return cursor;
+    }
+    
+    public Cursor selectAll() throws SQLException {
+    	Log.v(TAG,"selectAll() ");
+        Cursor cursor = db.query(true,TABLE_NAME, 
+                                new String[] {KEY_ROWID, KEY_MODE, KEY_LIST_DATA},
+                                null, 
+                                null,null, null, null, null);
+        
+        if (cursor != null) { cursor.moveToFirst(); }
+        
+        return cursor;
+    }
+    
+    
+}
---
+++ TenVersion_0526/src/com/smartschool/tenversion/DBHelper.java
@@ -0,0 +1,37 @@
+package com.smartschool.tenversion;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+private static final String DATABASE_NAME = "tenversion.db";
+private static final int DATABASE_VERSION = 1;
+public static final String TABLE_NAME = "checklist_table";
+
+
+public static final String KEY_ROWID = "_id";
+public static final String KEY_MODE = "mode";
+public static final String KEY_LIST_DATA = "list_data";
+	
+	public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+	private static final String DATABASE_CREATE =
+			"CREATE TABLE "+TABLE_NAME+" ("+
+			KEY_ROWID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+
+			KEY_MODE+" TEXT NOT NULL, "+
+			KEY_LIST_DATA+" TEXT NOT NULL);";
+	
+    @Override
+    public void onCreate(SQLiteDatabase db) { 
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {        
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+}
