5e9f2ee3e847b47cb4bf37871146dcc579bfe815
myong <gyeong.mh@gmail.com>
2012-06-05 20:17:47 +0900

    modify FlipperView

--- TenVersion_0526/src/com/smartschool/tenversion/FlipperView.java
+++ TenVersion_0526/src/com/smartschool/tenversion/FlipperView.java
@@ -1,66 +1,59 @@
 package com.smartschool.tenversion;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
-public class FlipperView extends Activity{
-	  private static final int MOUSE_DRAG_SENSITIVITY = 30;
-	  private static final int MOUSE_MOVE_SENSITIVITY = 50;
-	  
-	  ViewFlipper flipper;
-	  RelativeLayout rv;
-	  float start;
-	  float end;
-	  
-	  @Override
-	  public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.flipper);
+public class FlipperView extends Activity  implements View.OnTouchListener {
+ 
+    ViewFlipper flipper;
+     
+     float xAtDown;
+     float xAtUp;
 
-	    flipper = (ViewFlipper) findViewById(R.id.view_flipper);
-	    flipper.setOnTouchListener(new View.OnTouchListener() {
-	      
-	      public boolean onTouch(View v, MotionEvent event) {
-	        if(v != flipper){ 
-	          Log.d("IIIII","v != flipper");
-	          return false;
-	        }
-	        if(event.getAction() == MotionEvent.ACTION_DOWN){
-	          start = event.getX();
-	          Log.d("IIIII","start = " + start);
-	          
-	        }else if(event.getAction() == MotionEvent.ACTION_UP){
-	          end = event.getX();
-	          Log.d("IIIII","end = " + end);
-	          if(start-end < -MOUSE_DRAG_SENSITIVITY){
-	            right();
-	            flipper.showNext();
-	          }else if(start-end > MOUSE_DRAG_SENSITIVITY){
-	            left();
-	            flipper.showPrevious();
-	          }  
-	        }else if(event.getAction() == MotionEvent.ACTION_MOVE){
-	          Log.d("IIIII","move = " + event.getX());
-	          if(start-event.getX()< - MOUSE_MOVE_SENSITIVITY){
-	            start = event.getX();
-	            right();
-	            flipper.showNext();
-	          }else if(start-event.getX() > MOUSE_MOVE_SENSITIVITY){
-	            start = event.getX();
-	            left();
-	            flipper.showPrevious();
-	          }  
-	        }
-	        return true;
-	      }
-	    });
-	  }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.flipper);
+
+		flipper = (ViewFlipper) findViewById(R.id.view_flipper);
+		flipper.setOnTouchListener(this);
+
+		// 동적으로 화면하나 추가
+		TextView tv = new TextView(this);
+		tv.setText("View 4\nDynamically added");
+		tv.setTextColor(Color.CYAN);
+		flipper.addView(tv);
+	}
+
+	public boolean onTouch(View v, MotionEvent event) {
+		if (v != flipper)
+			return false;
+
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			xAtDown = event.getX(); // 처음 터치 위치 저장
+		} else if (event.getAction() == MotionEvent.ACTION_UP) {
+			xAtUp = event.getX(); // 터치 끝 위치 저장
+
+			if (xAtUp < xAtDown) {
+				left();
+
+				flipper.showNext();
+			} else if (xAtUp > xAtDown) {
+				right();
+				flipper.showPrevious();
+			}
+		}
+		return true;
+	}
+
 	  private void left(){
 	    flipper.setInAnimation(AnimationUtils.loadAnimation(this,R.anim.push_left_in));
 	    flipper.setOutAnimation(AnimationUtils.loadAnimation(this,R.anim.push_left_out));
